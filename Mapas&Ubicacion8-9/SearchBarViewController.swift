01e5626217a66fc243a61014f2a651999202d682
Marco Alonso <marcoalonsorota@hotmail.com>
2020-11-18 08:13:30 -0600

    Primer Commit

---
+++ Mapas&Ubicacion8-9/SearchBarViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SearchBarViewController.swift
+//  Mapas&Ubicacion8-9
+//
+//  Created by marco alonso on 29/10/20.
+//
+
+import UIKit
+import MapKit
+
+class SearchBarViewController: UIViewController {
+
+    @IBOutlet weak var Buscador: UISearchBar!
+    @IBOutlet weak var Mapa: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Buscador.delegate = self
+
+    }
+
+}
+
+
+extension SearchBarViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        Buscador.resignFirstResponder()  //ocultar teclado
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(Buscador.text!) { (places: [CLPlacemark]?, error: Error?) in
+            
+            if error == nil {
+                //muestre la direccion o la anotacion
+                let place = places?.first
+                
+                print("Places : \(places!)")
+                
+                
+                let anotacion = MKPointAnnotation()
+                anotacion.coordinate = (place?.location?.coordinate)!
+                anotacion.title = self.Buscador.text
+                
+                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)  //Vista del mapa que tan cerca o lejos se mira
+                
+                let region = MKCoordinateRegion(center: anotacion.coordinate, span: span)
+                
+                self.Mapa.setRegion(region, animated: true)
+                self.Mapa.addAnnotation(anotacion)
+                self.Mapa.selectAnnotation(anotacion, animated: true)
+                
+
+                
+            } else {
+                
+                print("Ubicacion no encontrada!")
+                
+            }
+            
+            
+        }
+        
+    }
+    
+}
---
+++ Mapas&Ubicacion8-9/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Mapas&Ubicacion8-9
+//
+//  Created by marco alonso on 28/10/20.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    
+    var manager = CLLocationManager()
+    var latitud : CLLocationDegrees!
+    var longitud : CLLocationDegrees!
+    
+
+    @IBOutlet weak var Mapa: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest     //obtenga la mejor precision de ubicacion
+        manager.startUpdatingLocation()   //Empieza a actualizar la ubicacion del GPS
+    }
+
+    @IBAction func GetLocation(_ sender: UIBarButtonItem) {
+        let alerta = UIAlertController(title: "Ubicacion", message: "Las coordenadas de tu ubicacion son: ", preferredStyle: .alert)
+        
+        let actionAceptar = UIAlertAction(title: "Aceptar", style: .default) { (_) in
+            print("Coordenadas")
+            
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        let actionVerMas = UIAlertAction(title: "Ver mas...", style: .default) { (_) in
+            let localizacion = CLLocationCoordinate2DMake(self.latitud, self.longitud)
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: localizacion, span: span)
+            self.Mapa.setRegion(region, animated: true)
+            self.Mapa.showsUserLocation = true
+        }
+        
+        alerta.addAction(actionAceptar)
+        alerta.addAction(actionVerMas)
+        
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            
+            //print(locations.first!)
+            self.latitud = location.coordinate.latitude
+            self.longitud = location.coordinate.longitude
+        }
+    }
+    
+    
+    
+}
